ef1c7020a5ced6048df101df77a4273090076725
Dmitriy Blavatnik <amit27@yandex.com>
2019-09-06 16:08:58 +0300

    Closed #3 task

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 # to execute this script run 'ruby main.rb'
 
+require_relative 'require'
 require_relative 'account'
 
-@account = Account.new(:firefox, false)
+@account = Account.new(:firefox, true)
 @account.open('https://my.fibank.bg/oauth2-server/login?client_id=E_BANK')
 @account.collect
