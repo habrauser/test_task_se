666021fd9605fa268cfab6367dcabb2d0e542edc
ximidas <ximidas@ximidas.com>
2019-09-07 18:25:54 +0300

    Closed #7 task

--- main.rb
+++ main.rb
@@ -3,16 +3,12 @@
 require_relative 'modules/require'
 
 require_relative 'lib/account'
-require_relative 'lib/transaction'
+
 
 @browser = :firefox
-@headless = false
+@headless = true
 @open = 'https://my.fibank.bg/oauth2-server/login?client_id=E_BANK'
 
 @account = Account.new(@browser, @headless)
 @account.open(@open)
 @account.collect
-
-@transactions = Transaction.new(@browser, @headless)
-@transactions.open(@open)
-@transactions.collect
